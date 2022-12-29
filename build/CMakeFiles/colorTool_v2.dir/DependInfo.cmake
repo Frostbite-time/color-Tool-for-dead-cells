77d84a339ee456c95573de484c3366492561debc
Frostbite-time <81908334+Frostbite-time@users.noreply.github.com>
2022-12-29 13:04:12 +0800

    Add files via upload

---
+++ build/CMakeFiles/colorTool_v2.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "F:/Ctest/colorTools/coloringTool/src/colorTool_v2.cpp" "CMakeFiles/colorTool_v2.dir/src/colorTool_v2.cpp.obj" "gcc" "CMakeFiles/colorTool_v2.dir/src/colorTool_v2.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/colorTool_v2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/colorTool_v2.dir/src/colorTool_v2.cpp.obj"
+  "CMakeFiles/colorTool_v2.dir/src/colorTool_v2.cpp.obj.d"
+  "bin/colorTool_v2.exe"
+  "bin/colorTool_v2.exe.manifest"
+  "bin/colorTool_v2.pdb"
+  "libcolorTool_v2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/colorTool_v2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
